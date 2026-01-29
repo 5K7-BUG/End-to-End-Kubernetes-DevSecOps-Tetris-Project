0bf7ace98c7ab526a0f466de3a5c100f66520f3c
5K7-BUG <61589209+5K7-BUG@users.noreply.github.com>
2026-01-29 20:23:04 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,15 +1,16 @@
 terraform {
   backend "s3" {
-    bucket       = "dev-aman-tf-bucket"
+    bucket       = "terraform-backend-bot1"
     region       = "us-east-1"
     key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    use_lockfile = true
+    dynamodb_table = "state-lock-file"
     encrypt      = true
+    use_lockfile = true
   }
-  required_version = ">=1.14.0"
+  required_version = ">= 1.6.0"
   required_providers {
     aws = {
-      version = ">= 5.49.0"
+      version = ">= 6.23.0"
       source  = "hashicorp/aws"
     }
   }
