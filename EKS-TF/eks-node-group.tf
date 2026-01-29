b9802280830d63505c12c02885a7cbf5b3050340
5K7-BUG <61589209+5K7-BUG@users.noreply.github.com>
2026-01-30 01:43:00 +0530

    Update eks-node-group.tf

--- EKS-TF/eks-node-group.tf
+++ EKS-TF/eks-node-group.tf
@@ -6,9 +6,9 @@ resource "aws_eks_node_group" "eks-node-group" {
 
 
   scaling_config {
-    desired_size = 0
-    max_size     = 0
-    min_size     = 0
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
   }
 
   instance_types = ["t3.small"]
