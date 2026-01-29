a3e3757d38e4cbbcef67d3db402b2c5f15706ee4
5K7-BUG <61589209+5K7-BUG@users.noreply.github.com>
2026-01-29 20:21:41 +0530

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,12 +1,13 @@
 terraform {
   backend "s3" {
-    bucket       = "dev-aman-tf-bucket"
+    bucket       = "terraform-backend-bot1"
     region       = "us-east-1"
     key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
+    dynamodb_table = "state-lock-file"
     encrypt      = true
     use_lockfile = true
   }
-  required_version = ">=1.13.3"
+  required_version = ">= 1.6.0"
   required_providers {
     aws = {
       version = ">= 6.23.0"
